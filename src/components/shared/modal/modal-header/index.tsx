9f1d4c640433aace1ec8a8207703a85fb1d253f7
got1000 <jinhyg2103@naver.com>
2025-08-14 00:08:28 +0900

    모달 닫기 버튼 위치 수정

--- src/components/shared/modal/modal-header/index.tsx
+++ src/components/shared/modal/modal-header/index.tsx
@@ -15,17 +15,28 @@ export function ModalHeader({
   ...props
 }: ModalHeaderProps) {
   return (
-    <div className={cn("typo-title-1-bold p-modal flex flex-row items-center border-b border-border", className)} {...props}>
-      {closable && onClose && (
-        <button
-          onClick={onClose}
-          className="relative text-gray-400 mr-[8px] cursor-pointer"
-          aria-label="Close"
-        >
-          <X className="h-5 w-5" />
-        </button>
+    <div
+      className={cn(
+        "typo-title-1-bold p-modal flex flex-row justify-between items-center border-b border-border",
+        className,
       )}
-      {children}
+      {...props}
+    >
+      <div className={cn("flex flex-1")}>{children}</div>
+      {closable && onClose && (
+        <div className={cn("flex align-center justify-center flex-shrink-0")}>
+          <button
+            onClick={onClose}
+            className={cn(
+              "relative w-[24px] h-[24px] text-gray-400 cursor-pointer rounded-4",
+              "hover:text-[rgba(0,0,0,0.88)] hover:bg-[rgba(0,0,0,0.06)] transition-all duration-200",
+            )}
+            aria-label="Close"
+          >
+            <X className="h-5 w-5 mx-auto" />
+          </button>
+        </div>
+      )}
     </div>
   );
 }
