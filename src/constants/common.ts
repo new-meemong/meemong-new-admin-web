18e87d0cfa13f40432575bc5c5ea92a257e637b5
got1000 <jinhyg2103@naver.com>
2025-08-21 00:02:57 +0900

    닉네임(일치) 옵션 추가

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,5 +1,6 @@
 export const SEARCH_TYPE_OPTIONS: { value: string; label: string }[] = [
   { value: "UID", label: "uuid" },
   { value: "NAME", label: "닉네임" },
+  { value: "NAME_EQUALS", label: "닉네임(일치)" },
   { value: "PHONE", label: "전화번호" },
 ];
